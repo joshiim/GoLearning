d81c5fe49262b360aa3132d35dab081007261a3a
Santosh Joshi <santoshjoshi1907@gmail.com>
2020-06-19 11:52:13 +0530

    just hello

--- hello.go
+++ hello.go
@@ -2,4 +2,17 @@ package main
 import "fmt"
 func main() {
 	fmt.Println("hello world facebook  @yo yo")
+	letsgo()
+  for i :=0;i<100;i++{
+  	if(i%10==0) {
+  	fmt.Println(i)
+	}
+  }
+ rest()
+}
+func letsgo(){
+	fmt.Println("we are in lets go function = + ")
+}
+func rest(){
+	fmt.Println("rest .......++++++--")
 }
